7ce52d93011ecbb377cdc240bd6b737663492896
13787104981 <1410796375@qq.com>
2020-04-23 08:09:15 +0800

    2020-04-23 提交测试：新增gmall-demo模块

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gmall
+# gmall-demo 本地测试 2020-04-23
---
+++ gmall-demo/src/main/java/com/gmall/gmalldemo/GmallDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gmall.gmalldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallDemoApplication.class, args);
+	}
+
+}
